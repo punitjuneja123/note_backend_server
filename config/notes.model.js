d8faa5080a75a3e0209b939431361d70e11e6748
punitjuneja123 <112753502+punitjuneja123@users.noreply.github.com>
2023-01-15 22:09:47 +0530

    Update notes.model.js
    title spell changed

--- config/notes.model.js
+++ config/notes.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const noteSchema = mongoose.Schema(
   {
-    titile: String,
+    title: String,
     note: String,
     category: String,
     userID: String,
